a533da5707ddaf261b03d288be26e1dab80061a9
FSEVERI\depaula2995 <FSEVERI\depaula2995@lmatbi-04.fseveri.it>
2016-03-10 10:55:03 +0100

    n

--- src/java/it/severi/gdtrsuper/controllers/EventoController.java
+++ src/java/it/severi/gdtrsuper/controllers/EventoController.java
@@ -18,10 +18,8 @@ import po.ManageDatabase;
  */
 @Controller
 public class EventoController {
-  //ManageDatabase db = new ManageDatabase();
-        ManageDatabase db = new ManageDatabase();
+    ManageDatabase db = new ManageDatabase();
 
-    
     @RequestMapping(value="/evento",method=RequestMethod.GET)
     public String index(ModelMap map, @RequestParam(value="id", required=true)int id){
         map.put("evento", db.getEventoById(id));
